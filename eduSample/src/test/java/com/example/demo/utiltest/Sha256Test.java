e8027021b3f970bd53bb20b6b6f24f955e056693
seongyun.lim <seongyun.lim@e4net.net>
2022-01-09 07:21:02 +0900

    sha256 test

---
+++ eduSample/src/test/java/com/example/demo/utiltest/Sha256Test.java
@@ -0,0 +1,22 @@
+package com.example.demo.utiltest;
+
+import java.security.NoSuchAlgorithmException;
+
+import org.junit.jupiter.api.Test;
+
+import com.example.demo.utils.Sha256Util;
+
+import lombok.extern.slf4j.Slf4j;
+@Slf4j
+public class Sha256Test {
+	@Test
+	public void test() {
+		Sha256Util t = new Sha256Util();
+		String str = "str";
+		try {
+			log.debug("end : {}", t.encrypt(str));
+		} catch (NoSuchAlgorithmException e) {
+			log.debug("err: {}", e.getMessage());
+		}
+	}
+}
