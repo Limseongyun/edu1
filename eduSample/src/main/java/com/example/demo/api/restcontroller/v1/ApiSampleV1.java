45cadb2fa75a602b195c1ed670dab0418884254d
seongyun.lim <seongyun.lim@e4net.net>
2022-01-09 11:31:46 +0900

    apiv1v2 and jwt

--- eduSample/src/main/java/com/example/demo/api/restcontroller/v1/ApiSampleV1.java
+++ eduSample/src/main/java/com/example/demo/api/restcontroller/v1/ApiSampleV1.java
@@ -1,11 +1,13 @@
-package com.example.demo.api;
+package com.example.demo.api.restcontroller.v1;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class ApiSample {
-	@GetMapping("/api/v1/sample")
+@RequestMapping("/api/v1")
+public class ApiSampleV1 {
+	@GetMapping("/sample")
 	public String sample() {
 		return "sample";
 	}
