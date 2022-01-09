8bd1f3422ff873ffdf71a6e2eb42b097fa6951d9
seongyun.lim <seongyun.lim@e4net.net>
2022-01-09 15:36:57 +0900

    base, basetime 분기

--- eduSample/src/main/java/com/example/demo/model/entity/BaseTime.java
+++ eduSample/src/main/java/com/example/demo/model/entity/BaseTime.java
@@ -6,20 +6,28 @@ import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 
+import org.hibernate.annotations.ColumnDefault;
 import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import lombok.Getter;
 
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-public class Base extends BaseTime {
-	@CreatedBy
-	@Column(name = "frst_regist_user_sn", nullable = false)
-	private Long frstRegistUserSn;
+public class BaseTime {
+	@Column(name = "use_yn")
+	@ColumnDefault("'Y'")
+	private String useYn;
 	
-	@LastModifiedBy
-	@Column(name = "last_change_user_sn", nullable = false)
-	private Long lastChangeUserSn;
+	@CreatedDate
+	@Column(name = "frst_regist_dt", nullable = false)
+	private LocalDateTime frstRegistDt;
+	
+	@LastModifiedDate
+	@Column(name = "last_change_dt", nullable = false)
+	private LocalDateTime lastChangeDt;
 }
