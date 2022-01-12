182cfc389245a7677d150b6e5ecf6c03eff2ce48
seongyun.lim <seongyun.lim@e4net.net>
2022-01-12 11:37:09 +0900

    웹에서 온 에러는 ErrorContorller에서 다시 받게 던져준다.

--- eduSample/src/main/java/com/example/demo/config/exception/MyRestControllerAdvice.java
+++ eduSample/src/main/java/com/example/demo/config/exception/MyRestControllerAdvice.java
@@ -14,13 +14,18 @@ public class MyRestControllerAdvice {
 	public RVO<String> err(Exception ex, HttpServletResponse resp, HttpServletRequest req) {
 		//Object errCode = req.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
 		//log.debug("{}, {}, {}", req, resp, errCode);
+		//만약 웹에서 온경우라면 ErrorController에서 다시 받도록하기위하여 exception을 다시 발생시켜준다.
+		if(req.getRequestURI().startsWith("/public")) {
+			throw new RuntimeException("오류발생");
+		}
+		
 		if(ex instanceof RuntimeException) {
 			//TODO: 익셉션별 response 부여 고려?
 			resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
 		} else {
 			resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
 		}
-		//ex.printStackTrace();
+		ex.printStackTrace();
 		return RVO.<String>builder().code(ApiCode.DEFAULT_ERR).msg("오류가 발생했 습니다.").data(ex.getMessage()).build();
 	}
 }
