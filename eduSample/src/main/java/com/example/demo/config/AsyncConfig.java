b07d1e6c1e50585efb0050259570c8db233337a9
seongyun.lim <seongyun.lim@e4net.net>
2022-01-12 11:58:07 +0900

    AsyncConfig

---
+++ eduSample/src/main/java/com/example/demo/config/AsyncConfig.java
@@ -0,0 +1,21 @@
+package com.example.demo.config;
+
+import java.util.concurrent.Executor;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurerSupport;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+public class AsyncConfig extends AsyncConfigurerSupport{
+	@Override
+	public Executor getAsyncExecutor() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(5);
+		executor.setMaxPoolSize(30);
+		executor.setQueueCapacity(50);
+		executor.setThreadNamePrefix("EDU-ASYNC-");
+		executor.initialize();
+		return executor;
+	}
+}
