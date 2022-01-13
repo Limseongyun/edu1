0acca05f3b13296e90dfb15ff4237bb1be95ea75
seongyun.lim <seongyun.lim@e4net.net>
2022-01-13 19:36:41 +0900

    머니잔고

--- eduSample/src/main/java/com/example/demo/mvc/model/entity3/MemberMoney.java
+++ eduSample/src/main/java/com/example/demo/mvc/model/entity3/MemberMoney.java
@@ -1,5 +1,39 @@
 package com.example.demo.mvc.model.entity3;
 
-public class MemberMoney {
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.example.demo.mvc.model.entity.Base;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "tb_memb_money", indexes = @Index(columnList = "login_no"))
+//@SequenceGenerator(name = "memLoginHst_seq", allocationSize = 1, initialValue = 1, sequenceName = "memLoginHst_seq")
+@DynamicInsert
+@DynamicUpdate
+@EntityListeners(AuditingEntityListener.class)
+//@Entity
+public class MemberMoney extends Base{
+	@Id@Column(name = "memb_no")
+	private Member membNo;
+	
+	@Column(name = "money_blce", length = 15)
+	@ColumnDefault("0")
+	private Long moneyBlce;
 }
